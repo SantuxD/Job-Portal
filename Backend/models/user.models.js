2dc8e4bec24942c483493db1e0e4a71d12ad5c49
Santu Mondal <mondalsantu104@gmail.com>
2025-10-16 01:18:22 +0530

    create user model

---
+++ Backend/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Recruiter"],
+      default: "Student",
+      required: true,
+    },
+    profile: {
+      bio: {
+        type: String,
+      },
+      skills: {
+        type: [String],
+        default: [],
+      },
+      resume: {
+        type: String,
+      },
+      company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+      },
+      profilePicture: {
+        type: String,
+        default: "",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
