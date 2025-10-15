61e4062257bc1d4659b7645a089202dad4c5d156
Santu Mondal <mondalsantu104@gmail.com>
2025-10-15 23:46:04 +0530

    create student model

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -2,14 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    console.log("MongoDB connected successfully ✅");
+    await mongoose.connect(process.env.MONGO_URI)
+         console.log("MongoDB connected successfully ✅")
     } catch (error) {
     console.error("MongoDB connection error:❌", error);
-    process.exit(1);
   }
 };
 export default connectDB;
