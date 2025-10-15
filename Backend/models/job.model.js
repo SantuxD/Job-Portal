f860e642356dbb58da01662c7dfd6487b923da71
Santu Mondal <mondalsantu104@gmail.com>
2025-10-16 01:23:55 +0530

    Create job Model

--- Backend/models/job.model.js
+++ Backend/models/job.model.js
@@ -1,1 +1,54 @@
 import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    requirements:{
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+    jobType: {
+        type: String,
+        enum: ["Full-time", "Part-time", "Contract", "Internship"],
+        required: true,
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+        required: true,
+    },
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    applicants: 
+        {
+            type: mongoose.Schema.Types.ObjectId,   
+            ref: "Application",
+            default: null
+        }
+    
+  },
+  { timestamps: true }
+);
+
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
+
